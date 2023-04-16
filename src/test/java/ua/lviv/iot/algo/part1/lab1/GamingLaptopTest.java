0d70bf0d452a6cfab164395b2ac2470cb693c08d
NazarPasternak21 <119297986+NazarPasternak21@users.noreply.github.com>
2023-04-16 18:59:40 +0300

    Update GamingLaptopTest.java

--- src/test/java/ua/lviv/iot/algo/part1/lab1/GamingLaptopTest.java
+++ src/test/java/ua/lviv/iot/algo/part1/lab1/GamingLaptopTest.java
@@ -1,35 +1,33 @@
 package ua.lviv.iot.algo.part1.lab1;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-class GamingLaptopTest {
+public class GamingLaptopTest {
 
     @Test
-    void testReplaceBattery() {
-        GamingLaptop laptop =new GamingLaptop("ASUS", 17.3, 32, 1024, 8, 100, "NVIDIA GeForce RTX 3080", 2);
-        Assertions.assertTrue(laptop.replaceBattery(10));
-        Assertions.assertEquals(10, laptop.getBatteryLife());
-        Assertions.assertEquals(100, laptop.getBatteryLevel());
+    public void testReplaceBattery() {
+        GamingLaptop laptop = new GamingLaptop("ASUS", 17.3, 32, 1024, 8, 100, "NVIDIA GeForce RTX 3080", 2);
+        boolean result = laptop.replaceBattery(10);
+        assertTrue(result);
+        assertEquals(10, laptop.getBatteryLife());
     }
+
     @Test
-    void testSetters() {
-        GamingLaptop laptop = new GamingLaptop();
-        laptop.setModel("Lenovo");
-        Assertions.assertEquals("Lenovo", laptop.getModel());
-        laptop.setScreenSize(15.6);
-        Assertions.assertEquals(15.6, laptop.getScreenSize());
-        laptop.setRam(16);
-        Assertions.assertEquals(16, laptop.getRam());
-        laptop.setStorage(1024);
-        Assertions.assertEquals(1024, laptop.getStorage());
-        laptop.setBatteryLife(8);
-        Assertions.assertEquals(8, laptop.getBatteryLife());
-        laptop.setBatteryLevel(100);
-        Assertions.assertEquals(100, laptop.getBatteryLevel());
-        laptop.setGraphicsProcessor("NVIDIA GeForce RTX 3080");
-        Assertions.assertEquals("NVIDIA GeForce RTX 3080", laptop.getGraphicsProcessor());
-        laptop.setFansNumber(2);
-        Assertions.assertEquals(2, laptop.getFansNumber());
+    public void testGraphicsProcessor() {
+        GamingLaptop laptop = new GamingLaptop("ASUS", 17.3, 32, 1024, 8, 100, "NVIDIA GeForce RTX 3080", 2);
+        assertEquals("NVIDIA GeForce RTX 3080", laptop.getGraphicsProcessor());
+        laptop.setGraphicsProcessor("new graphics");
+        assertEquals("new graphics", laptop.getGraphicsProcessor());
     }
+
+    @Test
+    public void testFansNumber() {
+        GamingLaptop laptop = new GamingLaptop("ASUS", 17.3, 32, 1024, 8, 100, "NVIDIA GeForce RTX 3080", 2);
+        assertEquals(2, laptop.getFansNumber());
+        laptop.setFansNumber(3);
+        assertEquals(3, laptop.getFansNumber());
+    }
+
 }
+
