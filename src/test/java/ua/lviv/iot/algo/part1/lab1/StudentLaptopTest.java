3da74d608bc8e878f521313b52aa9362628dad4b
NazarPasternak21 <119297986+NazarPasternak21@users.noreply.github.com>
2023-04-16 19:00:45 +0300

    Update StudentLaptopTest.java

--- src/test/java/ua/lviv/iot/algo/part1/lab1/StudentLaptopTest.java
+++ src/test/java/ua/lviv/iot/algo/part1/lab1/StudentLaptopTest.java
@@ -1,35 +1,29 @@
 package ua.lviv.iot.algo.part1.lab1;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-class StudentLaptopTest {
+public class StudentLaptopTest {
 
     @Test
-    void testReplaceBattery() {
+    public void testReplaceBattery() {
         StudentLaptop laptop = new StudentLaptop("Lenovo", 15.6, 16, 1024, 8, 100, "Intel Core i5-10300h", "China");
-        Assertions.assertTrue(laptop.replaceBattery(10));
-        Assertions.assertEquals(10, laptop.getBatteryLife());
-        Assertions.assertEquals(100, laptop.getBatteryLevel());
+        boolean result = laptop.replaceBattery(10);
+        assertTrue(result);
+        assertEquals(10, laptop.getBatteryLife());
     }
     @Test
-    void testSetters() {
-        StudentLaptop laptop = new StudentLaptop();
-        laptop.setModel("Lenovo");
-        Assertions.assertEquals("Lenovo", laptop.getModel());
-        laptop.setScreenSize(15.6);
-        Assertions.assertEquals(15.6, laptop.getScreenSize());
-        laptop.setRam(16);
-        Assertions.assertEquals(16, laptop.getRam());
-        laptop.setStorage(1024);
-        Assertions.assertEquals(1024, laptop.getStorage());
-        laptop.setBatteryLife(8);
-        Assertions.assertEquals(8, laptop.getBatteryLife());
-        laptop.setBatteryLevel(100);
-        Assertions.assertEquals(100, laptop.getBatteryLevel());
-        laptop.setProcessor("Intel Core i5-10300h");
-        Assertions.assertEquals("Intel Core i5-10300h", laptop.getProcessor());
-        laptop.setProducing_Country("China");
-        Assertions.assertEquals("China", laptop.getProducing_Country());
+    public void testProcessor() {
+        StudentLaptop laptop = new StudentLaptop("Lenovo", 15.6, 16, 1024, 8, 100, "Intel Core i5-10300h", "China");
+        assertEquals("Intel Core i5-10300h", laptop.getProcessor());
+        laptop.setProcessor("new processor");
+        assertEquals("new processor", laptop.getProcessor());
+    }
+    @Test
+    public void testProducingCountry() {
+        StudentLaptop laptop = new StudentLaptop("Lenovo", 15.6, 16, 1024, 8, 100, "Intel Core i5-10300h", "China");
+        assertEquals("China", laptop.getProducingCountry());
+        laptop.setProducingCountry("new producing country");
+        assertEquals("new producing country", laptop.getProducingCountry());
     }
 }
