673cd29d1365b1f7189389ac8f464e81a6561ee7
NazarPasternak21 <119297986+NazarPasternak21@users.noreply.github.com>
2023-04-16 18:59:56 +0300

    Update LaptopManagerTest.java

--- src/test/java/ua/lviv/iot/algo/part1/lab1/LaptopManagerTest.java
+++ src/test/java/ua/lviv/iot/algo/part1/lab1/LaptopManagerTest.java
@@ -1,57 +1,49 @@
 package ua.lviv.iot.algo.part1.lab1;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class LaptopManagerTest {
 
     private LaptopManager laptopManager;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         laptopManager = new LaptopManager();
+        laptopManager.addLaptopToList(new GamingLaptop("ASUS", 17.3, 32, 1024, 8, 100, "NVIDIA GeForce RTX 3080", 2));
+        laptopManager.addLaptopToList(new GamingLaptop("ACER", 15.6, 8, 512, 6, 100, "NVIDIA GeForce RTX 3050 Ti", 2));
+        laptopManager.addLaptopToList(new Ultrabook("ASUS", 15.6, 8, 512, 12, 100, 1.8, 1.7));
+        laptopManager.addLaptopToList(new Ultrabook("Lenovo", 15.6, 16, 1024, 12, 100, 1.8, 1.7));
+        laptopManager.addLaptopToList(new OfficeLaptop("Lenovo", 17.3, 16, 1024, 6, 100, "black", 25000));
+        laptopManager.addLaptopToList(new OfficeLaptop("ACER", 15.6, 8, 512, 8, 1002, "white", 16000));
+        laptopManager.addLaptopToList(new StudentLaptop("Lenovo", 15.6, 16, 1024, 8, 100, "Intel Core i5-10300h", "China"));
+        laptopManager.addLaptopToList(new StudentLaptop("ASUS", 17.3, 32, 1024, 12, 100, "Intel Core i7-1165G7 ", "China"));
     }
 
     @Test
-    public void testAddLaptop() {
-        Laptop laptop = new GamingLaptop("ASUS", 17.3, 32, 1024, 8, 100, "NVIDIA GeForce RTX 3080", 2);
-        laptopManager.addLaptop(laptop);
-
-        List<Laptop> laptops = laptopManager.getLaptops();
-        assertNotNull(laptops);
-        assertEquals(1, laptops.size());
-        assertEquals(laptop, laptops.get(0));
+    void testFindAllWithSameModel() {
+        List<Laptop> laptops = laptopManager.findAllWithSameModel("ASUS");
+        Assertions.assertEquals(3, laptops.size());
     }
 
     @Test
-    public void testFindAllWithSameModel() {
-        Laptop asusLaptop1 = new GamingLaptop("ASUS", 17.3, 32, 1024, 8, 100, "NVIDIA GeForce RTX 3080", 2);
-        Laptop asusLaptop2 = new Ultrabook("ASUS", 15.6, 8, 512, 12, 100, 1.8, 1.7);
-
-        laptopManager.addLaptop(asusLaptop1);
-        laptopManager.addLaptop(asusLaptop2);
-
-        laptopManager.findAllWithSameModel("ASUS");
+    void testFindAllWithSameRam() {
+        List<Laptop> laptops = laptopManager.findAllWithSameRam(16);
+        Assertions.assertEquals(3, laptops.size());
     }
 
     @Test
-    public void testFindAllWithSameRam() {
-        Laptop laptop1 = new GamingLaptop("ASUS", 17.3, 32, 1024, 8, 100, "NVIDIA GeForce RTX 3080", 2);
-        Laptop laptop2 = new Ultrabook("ASUS", 15.6, 16, 1024, 12, 100, 1.8, 1.7);
-
-        laptopManager.addLaptop(laptop1);
-        laptopManager.addLaptop(laptop2);
-
-        laptopManager.findAllWithSameRam(16);
+    void testFindLaptopsWith16GBRam() {
+        List<Laptop> laptops = laptopManager.findLaptopsWith16GBRam();
+        Assertions.assertEquals(3, laptops.size());
     }
 
     @Test
-    public void testRun() {
-        laptopManager.run();
+    void testFindAsusLaptops() {
+        List<Laptop> laptops = laptopManager.findAsusLaptops();
+        Assertions.assertEquals(3, laptops.size());
     }
 }
