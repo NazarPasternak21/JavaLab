c1976d80d1757206663205ebca6779a36892e7c2
NazarPasternak21 <119297986+NazarPasternak21@users.noreply.github.com>
2023-04-16 19:02:50 +0300

    Update StudentLaptop.java

--- src/main/java/ua/lviv/iot/algo/part1/lab1/StudentLaptop.java
+++ src/main/java/ua/lviv/iot/algo/part1/lab1/StudentLaptop.java
@@ -1,25 +1,34 @@
 package ua.lviv.iot.algo.part1.lab1;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @ToString(callSuper = true)
-public class StudentLaptop extends Laptop {
-    private String Processor;
-    private String Producing_Country;
+public final class StudentLaptop extends Laptop {
 
-    public StudentLaptop(String model, double screenSize, int ram, int storage, int batteryLife, int batteryLevel,
-                         String Processor, String Producing_Country) {
-        super(model, screenSize, ram, storage, batteryLevel, batteryLife);
-        this.Processor = Processor;
-        this.Producing_Country = Producing_Country;
+    private String processor;
+
+    private String producingCountry;
+
+    public StudentLaptop(final String model, final double screenSize, final int ram, final int storage,
+                         final int batteryLife, final int batteryLevel, final String processor,
+                         final String producingCountry) {
+        super(model, screenSize, ram, storage, batteryLife, batteryLevel);
+        this.processor = processor;
+        this.producingCountry = producingCountry;
     }
 
     @Override
-    public boolean replaceBattery(int capacityInHours) {
-        this.setBatteryLife(capacityInHours);
-        this.setBatteryLevel(100);
+    public boolean replaceBattery(final int newCapacity) {
+        this.setBatteryLife(newCapacity);
+        this.setBatteryLevel(BATTERY_LEVEL_FULL);
         return true;
     }
+
+    private static final int BATTERY_LEVEL_FULL = 100;
 }
