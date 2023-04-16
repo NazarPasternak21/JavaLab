f7502693e78f8b4516466984ec2748b01c1f65a5
NazarPasternak21 <119297986+NazarPasternak21@users.noreply.github.com>
2023-04-16 19:00:34 +0300

    Update OfficeLaptopTest.java

--- src/test/java/ua/lviv/iot/algo/part1/lab1/OfficeLaptopTest.java
+++ src/test/java/ua/lviv/iot/algo/part1/lab1/OfficeLaptopTest.java
@@ -1,35 +1,29 @@
 package ua.lviv.iot.algo.part1.lab1;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-class OfficeLaptopTest {
+public class OfficeLaptopTest {
 
     @Test
-    void testReplaceBattery() {
+    public void testReplaceBattery() {
         OfficeLaptop laptop = new OfficeLaptop("Lenovo", 17.3, 16, 1024, 6, 100, "black", 25000);
-        Assertions.assertTrue(laptop.replaceBattery(10));
-        Assertions.assertEquals(10, laptop.getBatteryLife());
-        Assertions.assertEquals(100, laptop.getBatteryLevel());
+        boolean result = laptop.replaceBattery(10);
+        assertTrue(result);
+        assertEquals(10, laptop.getBatteryLife());
     }
     @Test
-    void testSetters() {
-        OfficeLaptop laptop = new OfficeLaptop();
-        laptop.setModel("Lenovo");
-        Assertions.assertEquals("Lenovo", laptop.getModel());
-        laptop.setScreenSize(15.6);
-        Assertions.assertEquals(15.6, laptop.getScreenSize());
-        laptop.setRam(16);
-        Assertions.assertEquals(16, laptop.getRam());
-        laptop.setStorage(1024);
-        Assertions.assertEquals(1024, laptop.getStorage());
-        laptop.setBatteryLife(8);
-        Assertions.assertEquals(8, laptop.getBatteryLife());
-        laptop.setBatteryLevel(100);
-        Assertions.assertEquals(100, laptop.getBatteryLevel());
-        laptop.setColor("black");
-        Assertions.assertEquals("black", laptop.getColor());
-        laptop.setPrice(25000);
-        Assertions.assertEquals(25000, laptop.getPrice());
+    public void testColor() {
+        OfficeLaptop laptop = new OfficeLaptop("Lenovo", 17.3, 16, 1024, 6, 100, "black", 25000);
+        assertEquals("black", laptop.getLaptopColor());
+        laptop.setLaptopColor("new graphics");
+        assertEquals("new graphics", laptop.getLaptopColor());
+    }
+    @Test
+    public void testPrice() {
+        OfficeLaptop laptop = new OfficeLaptop("Lenovo", 17.3, 16, 1024, 6, 100, "black", 25000);
+        assertEquals(25000, laptop.getLaptopPrice(), 0.01);
+        laptop.setLaptopPrice(25000);
+        assertEquals(25000, laptop.getLaptopPrice(), 0.01);
     }
 }
