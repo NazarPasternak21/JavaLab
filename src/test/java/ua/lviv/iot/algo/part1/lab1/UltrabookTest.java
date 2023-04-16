504b758fa6cf9c60e9d94e39c82420e9f1db7139
NazarPasternak21 <119297986+NazarPasternak21@users.noreply.github.com>
2023-04-16 19:00:57 +0300

    Update UltrabookTest.java

--- src/test/java/ua/lviv/iot/algo/part1/lab1/UltrabookTest.java
+++ src/test/java/ua/lviv/iot/algo/part1/lab1/UltrabookTest.java
@@ -1,34 +1,29 @@
 package ua.lviv.iot.algo.part1.lab1;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-class UltrabookTest {
+public class UltrabookTest {
 
     @Test
-    void testReplaceBattery() {
+    public void testReplaceBattery() {
         Ultrabook laptop = new Ultrabook("ASUS", 15.6, 8, 512, 12, 100, 1.8, 1.7);
-        Assertions.assertFalse(laptop.replaceBattery(10));
+        boolean result = laptop.replaceBattery(10);
+        assertFalse(result);
+        assertEquals(100, laptop.getBatteryLife());
     }
-
     @Test
-    void testSetters() {
-        Ultrabook laptop = new Ultrabook();
-        laptop.setModel("ASUS");
-        Assertions.assertEquals("ASUS", laptop.getModel());
-        laptop.setScreenSize(15.6);
-        Assertions.assertEquals(15.6, laptop.getScreenSize());
-        laptop.setRam(8);
-        Assertions.assertEquals(8, laptop.getRam());
-        laptop.setStorage(512);
-        Assertions.assertEquals(512, laptop.getStorage());
-        laptop.setBatteryLife(12);
-        Assertions.assertEquals(12, laptop.getBatteryLife());
-        laptop.setBatteryLevel(100);
-        Assertions.assertEquals(100, laptop.getBatteryLevel());
-        laptop.setWeight(1.8);
-        Assertions.assertEquals(1.8, laptop.getWeight());
-        laptop.setThickness(1.7);
-        Assertions.assertEquals(1.7, laptop.getThickness());
+    public void testWeight() {
+        Ultrabook laptop = new Ultrabook("ASUS", 15.6, 8, 512, 12, 100, 1.8, 1.7);
+        assertEquals(1.8, laptop.getWeight(), 0.01);
+        laptop.setWeight(1.9);
+        assertEquals(1.9, laptop.getWeight(), 0.01);
+    }
+    @Test
+    public void testThickness() {
+        Ultrabook laptop = new Ultrabook("ASUS", 15.6, 8, 512, 12, 100, 1.8, 1.7);
+        assertEquals(1.7, laptop.getThickness(), 0.01);
+        laptop.setThickness(1.8);
+        assertEquals(1.8, laptop.getThickness(), 0.01);
     }
 }
