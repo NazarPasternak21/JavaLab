2839d919761bb914d5bf1cb6ee8d082a6c7d5de8
NazarPasternak21 <119297986+NazarPasternak21@users.noreply.github.com>
2023-04-24 12:22:51 +0300

    Update LaptopWriter.java

--- src/main/java/ua/lviv/iot/algo/part1/lab1/LaptopWriter.java
+++ src/main/java/ua/lviv/iot/algo/part1/lab1/LaptopWriter.java
@@ -1,47 +1,38 @@
 package ua.lviv.iot.algo.part1.lab1;
 
+import ua.lviv.iot.algo.part1.lab1.models.Laptop;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class LaptopWriter {
 
     public static final String LAPTOP_FILE_NAME = "laptops.csv";
     public static final int CHUNK_SIZE = 100;
 
-    public void writeToFile(List<Laptop> laptops) throws IOException {
+    public void writeToFile(final List<Laptop> laptops) throws IOException {
         if (laptops == null || laptops.isEmpty()) {
-            throw new IllegalArgumentException("Список ноутбуків не може бути порожнім або null");
+            throw new IllegalArgumentException(
+                    "Список ноутбуків не може бути порожнім або null");
         }
 
+        laptops.sort(Comparator.comparing(laptop -> laptop.getClass().getSimpleName()));
+
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(LAPTOP_FILE_NAME))) {
-            Map<Class<? extends Laptop>, String> headersMap = new HashMap<>();
-
-            for (int i = 0; i < laptops.size(); i += CHUNK_SIZE) {
-                int blockEndIndex = Math.min(i + CHUNK_SIZE, laptops.size());
-                List<Laptop> laptopsBlock = new ArrayList<>(laptops.subList(i, blockEndIndex));
-
-                for (Laptop laptop : laptopsBlock) {
-                    Class<? extends Laptop> laptopClass = laptop.getClass();
-                    if (!headersMap.containsKey(laptopClass)) {
-                        String headers = laptop.getHeaders();
-                        writer.write(headers);
-                        writer.newLine();
-                        headersMap.put(laptopClass, headers);
-                    }
-                    String csv = laptop.toCSV();
-                    writer.write(csv);
+            String prevouslyWrittenClass = null;
+            for (var laptop : laptops) {
+                if (laptop.getClass().getSimpleName() != prevouslyWrittenClass) {
+                    writer.write(laptop.getHeaders());
                     writer.newLine();
+                    prevouslyWrittenClass = laptop.getClass().getSimpleName();
                 }
-                writer.newLine(); // Додатковий рядок між блоками ноутбуків
+                writer.write(laptop.toCSV());
+                writer.newLine();
             }
-            System.out.println("Запис успішно завершено.");
         } catch (IOException e) {
-            System.out.println("Помилка запису у файл: " + e.getMessage());
+            e.printStackTrace();
             throw e;
         }
     }
